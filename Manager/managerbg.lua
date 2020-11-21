c61ff5a2360a267cfbd78177472c0d67a78f6a98
Claire Clark <17522030+augustclear@users.noreply.github.com>
2020-11-21 15:26:42 -0600

    scroll 3

--- Manager/managerbg.lua
+++ Manager/managerbg.lua
@@ -11,12 +11,11 @@ while true do
     --Go to new line
     ty = ty + 1
     term.setCursorPos(tx,ty)
-
+    manlib.get_heartbeats()
     --Scroll if past screen
     tx,ty = term.getCursorPos()
     if ty == my then
         term.scroll(1)
         ty = ty - 1
     end
-    manlib.get_heartbeats()
 end
