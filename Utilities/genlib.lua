e881d0c154346b0f24165725cfd0ee4736c95470
Claire Clark <17522030+augustclear@users.noreply.github.com>
2020-11-21 23:40:27 -0600

    additional cache removing header things

--- Utilities/genlib.lua
+++ Utilities/genlib.lua
@@ -2,7 +2,7 @@
 
 local function nuget(url,filename)
     local f = io.open(filename,"w")
-    local tmp = http.get("url",{"Cache-Control: no-cache"})
+    local tmp = http.get("url",{"Cache-Control: no-cache","Cache-Control: no-store","Cache-Control: max-age=0"})
     if tmp == nil then
         print("[Can't connect to git...]")
     else
