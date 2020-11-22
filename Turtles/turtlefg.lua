406b71cfcd5912662517932e8e9ac00c2300f59e
Claire Clark <17522030+augustclear@users.noreply.github.com>
2020-11-22 00:47:49 -0600

    test 2

--- Turtles/turtlefg.lua
+++ Turtles/turtlefg.lua
@@ -3,10 +3,10 @@ local command = nil;
 
 --Original Test Position 10,40,240
 print("Starting Test")
-
-w.go_to(15,40,240)
 w.go_to(10,40,240)
-w.go_to(5,40,240)
+--w.go_to(15,40,240)
+--w.go_to(10,40,240)
+--w.go_to(5,40,240)
 --term.setCursorPos(1,1)
 --term.write("Awaiting orders...")
 
