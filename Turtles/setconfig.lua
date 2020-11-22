8e6e90e5d14943deb0a1257d9a6d695abaa1813a
Claire Clark <17522030+augustclear@users.noreply.github.com>
2020-11-22 17:04:25 -0600

    = to ==

--- Turtles/setconfig.lua
+++ Turtles/setconfig.lua
@@ -3,8 +3,8 @@ local w = require("lib.turtlelib")
 
 w.read_config()
 
-if args[1] = "home" then
+if args[1] == "home" then
     w.write_config_value("home",{args[2],args[3],args[4]})
-elseif args[1] = "fuel_stop" then
+elseif args[1] == "fuel_stop" then
     w.write_config_value("fuel_stop",{args[2],args[3],args[4]})
 end
